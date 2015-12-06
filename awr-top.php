8ba115b7d259acfdfa3d100dc9906f539b90c955
Vasiliy Aboymov <aboymov@gmail.com>
2015-12-07 02:24:05 +0300

    faster query?

--- awr-top.php
+++ awr-top.php
@@ -29,7 +29,10 @@ SQL;
    $min_snap_id = $snapshots["MIN_SNAP_ID"][0];
    $max_snap_id = $snapshots["MAX_SNAP_ID"][0];
 
-   $query = <<<SQL
+   if ($_POST['type'] !== 'top-sql') {
+
+      $start_time = microtime(true);
+      $query = <<<SQL
 SELECT count(*) activity
   FROM dba_hist_active_sess_history
  WHERE snap_id between :min_snap_id and :max_snap_id
@@ -37,16 +40,27 @@ SELECT count(*) activity
    AND instance_number = 1 {$query_mod3} {$query_mod1}
 SQL;
 
-   $statement = oci_parse($connect, $query);
+      $statement = oci_parse($connect, $query);
 
-   oci_bind_by_name($statement, ':dbid', $_POST['dbid']);
-   oci_bind_by_name($statement, ':min_snap_id', $min_snap_id);
-   oci_bind_by_name($statement, ':max_snap_id', $max_snap_id);
+      oci_bind_by_name($statement, ':dbid', $_POST['dbid']);
+      oci_bind_by_name($statement, ':min_snap_id', $min_snap_id);
+      oci_bind_by_name($statement, ':max_snap_id', $max_snap_id);
 
-   oci_execute($statement);
-   oci_fetch_all($statement, $results);
+      oci_execute($statement);
+      oci_fetch_all($statement, $results);
 
-   $sum_activity = $results['ACTIVITY'][0];
+      print "<pre>";
+      print_r($query);
+      print "</pre>";
+
+      $end_time = microtime(true);
+
+      print "Elapsed time: ".round($end_time - $start_time,2) . "<br>";
+
+      $sum_activity = $results['ACTIVITY'][0];
+   } else {
+      $sum_activity = '';
+   }
 
   if ($_POST['type'] === 'top-sql') {
 
@@ -75,6 +89,29 @@ SELECT h1.sql_id, h1.sql_opcode, NVL(h2.{$query_mod2},'CPU') wait_class, round(C
    AND instance_number = 1
  GROUP BY h1.sql_id, h1.sql_opcode, nvl(h2.{$query_mod2},'CPU'), n
  order by n desc
+SQL;
+
+$query = <<<SQL
+SELECT *
+  FROM (SELECT sql_id, sql_opcode, wait_class, n, total, total_by_sql_id,
+               round(n/total*100,2) percent,
+               dense_rank() over (order by total_by_sql_id desc) as sql_rank
+          FROM (SELECT sql_id, sql_opcode, NVL({$query_mod2},'CPU') as wait_class, count(*) n,
+                       sum(count(*)) over () total,
+                       sum(count(*)) over (partition by sql_id,sql_opcode) total_by_sql_id
+                  FROM dba_hist_active_sess_history
+                 WHERE sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
+                   AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')
+                   AND snap_id > :min_snap_id
+                   AND snap_id < :max_snap_id
+                   AND dbid = :dbid
+                   AND instance_number = 1
+                   AND sql_id IS NOT NULL {$query_mod1}
+                 GROUP BY sql_id, sql_opcode, NVL({$query_mod2},'CPU')
+                 ORDER BY 6 DESC)
+        GROUP BY sql_id, sql_opcode,  wait_class, n, total, total_by_sql_id)
+ WHERE sql_rank <= 10
+ ORDER BY sql_rank
 SQL;
 
    } else if ($_POST['type'] === 'top-session') {
@@ -130,8 +167,18 @@ SQL;
 
    $statement = oci_parse($connect, $sql);
 
+   $start_time = microtime(true);
+
    oci_execute($statement);
    oci_fetch_all($statement, $results);
+
+   print "<pre>";
+   print_r($sql);
+   print "</pre>";
+
+   $end_time = microtime(true);
+
+   print "Elapsed time: ".round($end_time - $start_time,2) . "<br><br>";
 
    if ($_POST['type'] === 'top-sql') {
 
@@ -216,7 +263,8 @@ SQL;
             $top[$results["SQL_ID"][$i]]["EXECUTIONS"] = 'unavailable';
          }
 
-         $top[$results["SQL_ID"][$i]]["PERCENT_TOTAL"] = $results["N"][$i]/$sum_activity*100;
+         //$top[$results["SQL_ID"][$i]]["PERCENT_TOTAL"] = $results["N"][$i]/$sum_activity*100;
+         $top[$results["SQL_ID"][$i]]["PERCENT_TOTAL"] = $results["TOTAL_BY_SQL_ID"][$i]/$results["TOTAL"][$i]*100;
          $top[$results["SQL_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
       } elseif ($_POST['type'] === 'top-session') {
          $top[$results["SESSION_ID"][$i]]["PROGRAM"] = $results["PROGRAM"][$i];
