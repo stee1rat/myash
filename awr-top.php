f43f1b2060ed9e00308f9fc3bfdd6f80ef71aaf3
Vasiliy Aboymov <aboymov@gmail.com>
2015-12-07 21:43:26 +0300

    faster queries

--- awr-top.php
+++ awr-top.php
@@ -29,73 +29,13 @@ SQL;
    $min_snap_id = $snapshots["MIN_SNAP_ID"][0];
    $max_snap_id = $snapshots["MAX_SNAP_ID"][0];
 
-   if ($_POST['type'] !== 'top-sql') {
-
-      $start_time = microtime(true);
+   if ($_POST['type'] === 'top-sql') {
+      
       $query = <<<SQL
-SELECT count(*) activity
-  FROM dba_hist_active_sess_history
- WHERE snap_id between :min_snap_id and :max_snap_id
-   AND dbid = :dbid
-   AND instance_number = 1 {$query_mod3} {$query_mod1}
-SQL;
-
-      $statement = oci_parse($connect, $query);
-
-      oci_bind_by_name($statement, ':dbid', $_POST['dbid']);
-      oci_bind_by_name($statement, ':min_snap_id', $min_snap_id);
-      oci_bind_by_name($statement, ':max_snap_id', $max_snap_id);
-
-      oci_execute($statement);
-      oci_fetch_all($statement, $results);
-
-      print "<pre>";
-      print_r($query);
-      print "</pre>";
-
-      $end_time = microtime(true);
-
-      print "Elapsed time: ".round($end_time - $start_time,2) . "<br>";
-
-      $sum_activity = $results['ACTIVITY'][0];
-   } else {
-      $sum_activity = '';
-   }
-
-  if ($_POST['type'] === 'top-sql') {
-
-$query = <<<SQL
-SELECT h1.sql_id, h1.sql_opcode, NVL(h2.{$query_mod2},'CPU') wait_class, round(Count(*)/:sum_activity*100,2) PERCENT, n
-  FROM (SELECT *
-          FROM (SELECT sql_id, sql_opcode, count(*) n
-                  FROM dba_hist_active_sess_history
-                 WHERE sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
-                   AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')
-                   AND snap_id > :min_snap_id
-                   AND snap_id < :max_snap_id
-                   AND dbid = :dbid
-                   AND instance_number = 1
-                   AND sql_id IS NOT NULL {$query_mod1}
-                 GROUP BY sql_id, sql_opcode
-                 ORDER BY 3 DESC)
-         WHERE rownum <= 10 ) h1,
-       dba_hist_active_sess_history h2
- WHERE h1.sql_id = h2.sql_id {$query_mod1}
-   AND sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
-   AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')
-   AND snap_id > :min_snap_id
-   AND snap_id < :max_snap_id
-   AND dbid = :dbid
-   AND instance_number = 1
- GROUP BY h1.sql_id, h1.sql_opcode, nvl(h2.{$query_mod2},'CPU'), n
- order by n desc
-SQL;
-
-$query = <<<SQL
 SELECT *
   FROM (SELECT sql_id, sql_opcode, wait_class, n, total, total_by_sql_id,
                round(n/total*100,2) percent,
-               dense_rank() over (order by total_by_sql_id desc) as sql_rank
+               dense_rank() over (order by total_by_sql_id desc, sql_id desc) as rank
           FROM (SELECT sql_id, sql_opcode, NVL({$query_mod2},'CPU') as wait_class, count(*) n,
                        sum(count(*)) over () total,
                        sum(count(*)) over (partition by sql_id,sql_opcode) total_by_sql_id
@@ -110,75 +50,55 @@ SELECT *
                  GROUP BY sql_id, sql_opcode, NVL({$query_mod2},'CPU')
                  ORDER BY 6 DESC)
         GROUP BY sql_id, sql_opcode,  wait_class, n, total, total_by_sql_id)
- WHERE sql_rank <= 10
- ORDER BY sql_rank
+ WHERE rank <= 10
+ ORDER BY rank
 SQL;
 
    } else if ($_POST['type'] === 'top-session') {
 
    $query = <<<SQL
-SELECT h.*, h.user_id username
- FROM (SELECT h1.session_id || ',' ||  h1.session_serial# session_id, h2.program, nvl(h2.{$query_mod2},'CPU') wait_class,
-           user_id, round(count(*)/:sum_activity*100,2) PERCENT, n
-  FROM (SELECT *
-          FROM (SELECT session_id, session_serial#, count(*) n
-                  FROM dba_hist_active_sess_history
-                 WHERE sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
-                   AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')
-                   AND snap_id > :min_snap_id
-                   AND snap_id < :max_snap_id
-                   AND dbid = :dbid
-                   AND instance_number = 1 {$query_mod1}
-                 GROUP BY session_id, session_serial#
-                 ORDER BY 3 DESC)
-         WHERE rownum <= 10 ) h1,
-       dba_hist_active_sess_history h2
- WHERE h1.session_id = h2.session_id
-   AND h1.session_serial# = h2.session_serial#
-   AND sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
-   AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')
-   AND snap_id > :min_snap_id
-   AND snap_id < :max_snap_id
-   AND dbid = :dbid
-   AND instance_number = 1 {$query_mod1}
- GROUP BY h1.session_id, h1.session_serial#, h2.program, nvl(h2.{$query_mod2},'CPU'), n, user_id) h
-ORDER BY n DESC, session_id DESC, wait_class DESC
+SELECT *
+FROM (SELECT session_id, program, wait_class, user_id, n, total, total_by_sid,
+            round(n/total*100,2) percent,
+            dense_rank() over (order by total_by_sid desc, session_id desc) as rank
+       FROM (SELECT session_id || ',' || session_serial# session_id, 
+                    program, nvl({$query_mod2},'CPU') wait_class, user_id, count(*) n,
+                    sum(count(*)) over () total,
+                    sum(count(*)) over (partition by session_id  || ',' ||  session_serial#) total_by_sid
+               FROM dba_hist_active_sess_history
+              WHERE sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
+                AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')
+                AND snap_id > :min_snap_id
+                AND snap_id < :max_snap_id
+                AND dbid = :dbid
+                AND instance_number = 1 {$query_mod1}
+              GROUP BY session_id || ',' || session_serial#, program, nvl({$query_mod2},'CPU'), user_id
+              ORDER BY 7 desc, 1))
+WHERE rank <= 10
+ORDER BY rank
 SQL;
 
   }
 
    $start_time = microtime(true);
 
-  // $statement = oci_parse($connect, $query);
-  //
-  // oci_bind_by_name($statement, ':dbid', $_POST['dbid']);
-  // oci_bind_by_name($statement, ':min_snap_id', $min_snap_id, -1, OCI_B_INT);
-  // oci_bind_by_name($statement, ':max_snap_id', $max_snap_id, -1, OCI_B_INT);
-  // oci_bind_by_name($statement, ":start_date", $start_date);
-  // oci_bind_by_name($statement, ":end_date", $end_date);
-  // oci_bind_by_name($statement, ":sum_activity", $sum_activity);
-
    $sql = str_replace(':dbid', $_POST['dbid'], $query);
    $sql = str_replace(':min_snap_id', $min_snap_id, $sql);
    $sql = str_replace(':max_snap_id', $max_snap_id, $sql);
    $sql = str_replace(':start_date', "'".$start_date."'", $sql);
    $sql = str_replace(':end_date', "'".$end_date."'", $sql);
-   $sql = str_replace(':sum_activity', $sum_activity, $sql);
 
    $statement = oci_parse($connect, $sql);
-
-   $start_time = microtime(true);
+   
+   // oci_bind_by_name($statement, ':dbid', $_POST['dbid']);
+   // oci_bind_by_name($statement, ':min_snap_id', $min_snap_id, -1, OCI_B_INT);
+   // oci_bind_by_name($statement, ':max_snap_id', $max_snap_id, -1, OCI_B_INT);
+   // oci_bind_by_name($statement, ":start_date", $start_date);
+   // oci_bind_by_name($statement, ":end_date", $end_date);
+   // oci_bind_by_name($statement, ":sum_activity", $sum_activity);
 
    oci_execute($statement);
    oci_fetch_all($statement, $results);
-
-   print "<pre>";
-   print_r($sql);
-   print "</pre>";
-
-   $end_time = microtime(true);
-
-   print "Elapsed time: ".round($end_time - $start_time,2) . "<br><br>";
 
    if ($_POST['type'] === 'top-sql') {
 
@@ -242,6 +162,8 @@ SQL;
 
    if (sizeof($results["N"]) <= 0) {
       exit;
+   } else {
+      $sum_activity = $results["TOTAL"][0];
    }
 
    $top = array();
@@ -263,14 +185,13 @@ SQL;
             $top[$results["SQL_ID"][$i]]["EXECUTIONS"] = 'unavailable';
          }
 
-         //$top[$results["SQL_ID"][$i]]["PERCENT_TOTAL"] = $results["N"][$i]/$sum_activity*100;
          $top[$results["SQL_ID"][$i]]["PERCENT_TOTAL"] = $results["TOTAL_BY_SQL_ID"][$i]/$results["TOTAL"][$i]*100;
          $top[$results["SQL_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
       } elseif ($_POST['type'] === 'top-session') {
          $top[$results["SESSION_ID"][$i]]["PROGRAM"] = $results["PROGRAM"][$i];
          $top[$results["SESSION_ID"][$i]]["SESSION_ID"] = $results["SESSION_ID"][$i];
-         $top[$results["SESSION_ID"][$i]]["USERNAME"] = $results["USERNAME"][$i];
-         $top[$results["SESSION_ID"][$i]]["PERCENT_TOTAL"] = $results["N"][$i]/$sum_activity*100;
+         $top[$results["SESSION_ID"][$i]]["USERNAME"] = $results["USER_ID"][$i];
+         $top[$results["SESSION_ID"][$i]]["PERCENT_TOTAL"] = $results["TOTAL_BY_SID"][$i]/$results["TOTAL"][$i]*100;
          $top[$results["SESSION_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
       }
    }
