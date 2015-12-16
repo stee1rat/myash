f627c57208b00257f1074efb84b1cf865ac81eab
Vasiliy Aboymov <aboymov@gmail.com>
2015-12-16 19:52:00 +0300

    performance

--- ash-top.php
+++ ash-top.php
@@ -1,18 +1,14 @@
 <?php
-   include('connect.php');
-   include('query-mods.php');
 
-   if ($_POST['type'] === 'top-sql') {
-      include('sql-types.php');
-   }
+include('connect.php');
+include('query-mods.php');
 
-   $start_date = $_POST['startDate'];
-   $end_date   = $_POST['endDate'];
+if ($_POST['type'] === 'top-sql') {
+   
+   include('sql-types.php');
 
-   if ($_POST['type'] === 'top-sql') {
-
-      $query = <<<SQL
-SELECT s.sql_id, sql_opcode, wait_class, n, total, total_by_sql_id, percent, rank, 
+   $query = <<<SQL
+SELECT s.sql_id, sql_opcode, wait_class, n, total, total_by_sql_id, percent, rank,
        dbms_lob.substr(sql_text,1000,1) sql_text, SUM(executions) executions,
        ROUND(SUM(s.elapsed_time)/DECODE(SUM(s.executions),0,1,SUM(s.executions))/1e6, 2) avg_time
   FROM (SELECT sql_id, sql_opcode, wait_class, n, total, total_by_sql_id,
@@ -22,8 +18,8 @@ SELECT s.sql_id, sql_opcode, wait_class, n, total, total_by_sql_id, percent, ran
                        sum(count(*)) over () total,
                        sum(count(*)) over (partition by sql_id,sql_opcode) total_by_sql_id
                   FROM v\$active_session_history
-                 WHERE sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
-                   AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')  
+                 WHERE sample_time > to_date('{$_POST['startDate']}', 'DD.MM.YYYY HH24:MI:SS')
+                   AND sample_time < to_date('{$_POST['endDate']}', 'DD.MM.YYYY HH24:MI:SS')  
                    {$query_mod1} 
                    {$query_mod3} 
                  GROUP BY sql_id, sql_opcode, NVL({$query_mod2},'CPU')
@@ -36,120 +32,117 @@ SELECT s.sql_id, sql_opcode, wait_class, n, total, total_by_sql_id, percent, ran
  ORDER BY rank   
 SQL;
  
-   }
+}
 
-   if ($_POST['type'] === 'top-session') {
+if ($_POST['type'] === 'top-session') {
 
-      $query = <<<SQL
+   $query = <<<SQL
 SELECT h.*, u.username
-FROM (SELECT session_id, program, wait_class, user_id, n, total, total_by_sid,
-            round(n/total*100,2) percent,
-            dense_rank() over (order by total_by_sid desc, session_id desc) as rank
+FROM (SELECT session_id, program, wait_class, user_id, n, total, total_by_sid,             
+             round(n/total*100,2) percent,
+             dense_rank() over (order by total_by_sid desc, session_id desc) as rank
        FROM (SELECT session_id || ',' || session_serial# session_id, 
                     program, nvl({$query_mod2},'CPU') wait_class, user_id, count(*) n,
                     sum(count(*)) over () total,
                     sum(count(*)) over (partition by session_id  || ',' ||  session_serial#) total_by_sid
                FROM v\$active_session_history
-              WHERE sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
-                AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')
+              WHERE sample_time > to_date('{$_POST['startDate']}', 'DD.MM.YYYY HH24:MI:SS')
+                AND sample_time < to_date('{$_POST['endDate']}', 'DD.MM.YYYY HH24:MI:SS')
                 {$query_mod1}
               GROUP BY session_id || ',' || session_serial#, program, nvl({$query_mod2},'CPU'), user_id
-              ORDER BY 7 desc, 1))h,
+              ORDER BY 7 desc, 1)) h,
       dba_users u
 WHERE h.user_id = u.user_id and rank <= 10
 ORDER BY rank
 SQL;
 
-   }   
+}
 
-   $query = removeEmptyLines($query);
-   
-   $start_time = microtime(true);
+$start_time = microtime(true);
 
-   $statement = oci_parse($connect, $query);
-   oci_bind_by_name($statement, ":start_date", $start_date);
-   oci_bind_by_name($statement, ":end_date", $end_date);
-   oci_execute($statement);
-   oci_fetch_all($statement, $results);
+$query = removeEmptyLines($query);
+$statement = oci_parse($connect, $query);
+oci_execute($statement);
+oci_fetch_all($statement, $results);
 
-   if (sizeof($results["N"]) <= 0) {
-      exit;
-   } else {
-      $sum_activity = $results["TOTAL"][0];
-   }
-   
-   $top = array();
-   for ($i=0; $i<sizeof($results["N"]); $i++) {
-      if ($_POST['type'] === 'top-sql') {
-         $top[$results["SQL_ID"][$i]]["TEXT"] = $results["SQL_TEXT"][$i];
-         $top[$results["SQL_ID"][$i]]["SQL_ID"] = $results["SQL_ID"][$i];
-         $top[$results["SQL_ID"][$i]]["SQL_OPCODE"] = $results["SQL_OPCODE"][$i];
-         $top[$results["SQL_ID"][$i]]["AVG_TIME"] = $results["AVG_TIME"][$i];
-         $top[$results["SQL_ID"][$i]]["EXECUTIONS"] = $results["EXECUTIONS"][$i];
-         $top[$results["SQL_ID"][$i]]["PERCENT_TOTAL"] = $results["N"][$i]/$sum_activity*100;
-         $top[$results["SQL_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
-      } elseif ($_POST['type'] === 'top-session') {
-         $top[$results["SESSION_ID"][$i]]["PROGRAM"] = $results["PROGRAM"][$i];
-         $top[$results["SESSION_ID"][$i]]["SESSION_ID"] = $results["SESSION_ID"][$i];
-         $top[$results["SESSION_ID"][$i]]["USERNAME"] = $results["USERNAME"][$i];
-         $top[$results["SESSION_ID"][$i]]["PERCENT_TOTAL"] = $results["N"][$i]/$sum_activity*100;
-         $top[$results["SESSION_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
-      }
-   }
+if (sizeof($results["N"]) <= 0) {
+   exit;
+} else {
+   $sum_activity = $results["TOTAL"][0];
+}
 
-   print "<table class='output'>";
-
-   // Table headers
-   print "<thead><tr>";
-   if ($_POST['type'] === 'top-sql') {
-      print "<th align='left'>SQL ID</th>";
-      print "<th width='150px' align='left'>Activity</th>";
-      print "<th align='left' nowrap>SQL Type</th>";
-      print "<th align='left' nowrap>&nbsp;&nbsp;Executions</th>";
-      print "<th align='left' nowrap>&nbsp;&nbsp;Average Time</th>";
+$top = array();
+for ($i=0; $i<sizeof($results["N"]); $i++) {
+   if ($_POST['type'] === 'top-sql') {      
+      $top[$results["SQL_ID"][$i]]["SQL_ID"] = $results["SQL_ID"][$i];
+      $top[$results["SQL_ID"][$i]]["SQL_OPCODE"] = $results["SQL_OPCODE"][$i];
+      $top[$results["SQL_ID"][$i]]["AVG_TIME"] = $results["AVG_TIME"][$i];
+      $top[$results["SQL_ID"][$i]]["EXECUTIONS"] = $results["EXECUTIONS"][$i];
+      $top[$results["SQL_ID"][$i]]["TEXT"] = $results["SQL_TEXT"][$i];
+      $top[$results["SQL_ID"][$i]]["PERCENT_TOTAL"] = $results["TOTAL_BY_SQL_ID"][$i]/$results["TOTAL"][$i]*100;
+      $top[$results["SQL_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
    } elseif ($_POST['type'] === 'top-session') {
-      print "<th align='left' nowrap>SID,Serial#&nbsp;&nbsp;</th>";
-      print "<th width='150px' align='left'>Activity</th>";
-      print "<th align='left'>Username&nbsp;&nbsp;</th>";
-      print "<th align='left'>Program</th>";
+      $top[$results["SESSION_ID"][$i]]["PROGRAM"] = $results["PROGRAM"][$i];
+      $top[$results["SESSION_ID"][$i]]["SESSION_ID"] = $results["SESSION_ID"][$i];
+      $top[$results["SESSION_ID"][$i]]["USERNAME"] = $results["USERNAME"][$i];
+      $top[$results["SESSION_ID"][$i]]["PERCENT_TOTAL"] = $results["TOTAL_BY_SID"][$i]/$results["TOTAL"][$i]*100;
+      $top[$results["SESSION_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
    }
-   print "</tr></thead>";
+}
 
-   // Rest of the table
-   foreach ($top as $position) {
-      print "<tr>";
+print "<table class='output'>";
 
-      if ($_POST['type'] === 'top-sql') {
-         print "<td><a href='#' title='".htmlspecialchars($position["TEXT"]) ."'>".$position["SQL_ID"] . "</a>&nbsp;</td>";
-      } elseif ($_POST['type'] === 'top-session') {
-         print "<td>".$position["SESSION_ID"] . "</td>";
-      }
+// Table headers
+print "<thead><tr>";
+if ($_POST['type'] === 'top-sql') {
+   print "<th align='left'>SQL ID</th>";
+   print "<th width='150px' align='left'>Activity</th>";
+   print "<th align='left' nowrap>SQL Type</th>";
+   print "<th align='left' nowrap>&nbsp;&nbsp;Executions</th>";
+   print "<th align='left' nowrap>&nbsp;&nbsp;Average Time</th>";
+} elseif ($_POST['type'] === 'top-session') {
+   print "<th align='left' nowrap>SID,Serial#&nbsp;&nbsp;</th>";
+   print "<th width='150px' align='left'>Activity</th>";
+   print "<th align='left'>Username&nbsp;&nbsp;</th>";
+   print "<th align='left'>Program</th>";
+}
+print "</tr></thead>";
 
-      print "<td>";
-      print "<table width='100%'><tr>";
-      print "<td width='100%'>";
-      foreach ($position["WAIT_CLASS"] as $key => $value) {
-         print "<div style='background:".$_POST["eventColors"][$key].";width:$value%;float:left;'>&nbsp;</div>";
-      }
-      print "</td>";
-      print "<td><div style=''>".round($position["PERCENT_TOTAL"],2). "%</div></td>";
-      print "</td>";
-      print "</tr></table>";
+// Rest of the table
+foreach ($top as $position) {
+   print "<tr>";
 
-      if ($_POST['type'] === 'top-sql') {
-         print "<td nowrap align='left'>".get_sqltype($position["SQL_OPCODE"]) . "</td>";
-         print "<td nowrap align='right'>". $position["EXECUTIONS"] . "</td>";
-         print "<td nowrap align='right'>". number_format(round($position["AVG_TIME"],4),2,'.','') . "s</td>";
-      } elseif ($_POST['type'] === 'top-session') {
-         print "<td nowrap align='right'>". $position["USERNAME"] . "</td>";
-         print "<td nowrap>". $position["PROGRAM"] . "</td>";
-      }
-      print "</tr>";
+   if ($_POST['type'] === 'top-sql') {
+      print "<td><a href='#' title='".htmlspecialchars($position["TEXT"]) ."'>".$position["SQL_ID"] . "</a>&nbsp;</td>";
+   } elseif ($_POST['type'] === 'top-session') {
+      print "<td>".$position["SESSION_ID"] . "</td>";
    }
-   print "</table>";
 
-   $end_time = microtime(true);
+   print "<td>";
+   print "<table width='100%'><tr>";
+   print "<td width='100%'>";
+   foreach ($position["WAIT_CLASS"] as $key => $value) {
+      print "<div style='background:".$_POST["eventColors"][$key].";width:$value%;float:left;'>&nbsp;</div>";
+   }
+   print "</td>";
+   print "<td><div style=''>".round($position["PERCENT_TOTAL"],2). "%</div></td>";
+   print "</td>";
+   print "</tr></table>";
 
-   print "<div align='right'><font style='font-family: Tahoma,Verdana,Helvetica,sans-serif;font-size:9px' color='gray'>Total Sample Count: $sum_activity, Returned in: ".round($end_time - $start_time,2) . "s</font></div>";
+   if ($_POST['type'] === 'top-sql') {
+      print "<td nowrap align='left'>".get_sqltype($position["SQL_OPCODE"]) . "</td>";
+      print "<td nowrap align='right'>". $position["EXECUTIONS"] . "</td>";
+      print "<td nowrap align='right'>". number_format(round($position["AVG_TIME"],4),2,'.','') . "s</td>";
+   } elseif ($_POST['type'] === 'top-session') {
+      print "<td nowrap>". $position["USERNAME"] . "</td>";
+      print "<td nowrap>". $position["PROGRAM"] . "</td>";
+   }
+   print "</tr>";
+}
+print "</table>";
+
+$end_time = microtime(true);
+
+print "<div align='right'><font style='font-family: Tahoma,Verdana,Helvetica,sans-serif;font-size:9px' color='gray'>Total Sample Count: $sum_activity, Returned in: ".round($end_time - $start_time,2) . "s</font></div>";
 
 ?>
--- awr-top.php
+++ awr-top.php
@@ -1,33 +1,32 @@
 <?php
-   include('connect.php');
-   include('query-mods.php');
 
-   if ($_POST['type'] === 'top-sql') {
-      include('sql-types.php');
-   }
+include('connect.php');
+include('query-mods.php');
 
-   $start_date = $_POST['startDate'];
-   $end_date   = $_POST['endDate'];
-
-   $query = <<<SQL
+$query = <<<SQL
 SELECT min(snap_id) min_snap_id, max(snap_id) max_snap_id
   FROM dba_hist_snapshot
- WHERE trunc(begin_interval_time) = to_date(:day,'DD.MM.YYYY')
-   AND dbid = :dbid
+ WHERE begin_interval_time > to_date('{$_POST['startDate']}', 'DD.MM.YYYY HH24:MI:SS')
+   AND begin_interval_time < to_date('{$_POST['endDate']}', 'DD.MM.YYYY HH24:MI:SS')
+   AND dbid = {$_POST['dbid']}
 SQL;
 
-   $statement = oci_parse($connect, $query);
-   oci_bind_by_name($statement, ':dbid', $_POST['dbid']);
-   oci_bind_by_name($statement, ':day', $_POST['day']);
-   oci_execute($statement);
-   oci_fetch_all($statement, $snapshots);
+$statement = oci_parse($connect, $query);
+oci_execute($statement);
+oci_fetch_all($statement, $snapshots);
 
-   $min_snap_id = $snapshots["MIN_SNAP_ID"][0];
-   $max_snap_id = $snapshots["MAX_SNAP_ID"][0];
+if ($snapshots["MIN_SNAP_ID"][0] === null) {   
+   exit;
+}
 
-   if ($_POST['type'] === 'top-sql') {
-      
-      $query = <<<SQL
+$min_snap_id = $snapshots["MIN_SNAP_ID"][0];
+$max_snap_id = $snapshots["MAX_SNAP_ID"][0];
+
+if ($_POST['type'] === 'top-sql') {
+
+   include('sql-types.php');
+
+   $query = <<<SQL
 SELECT *
   FROM (SELECT sql_id, sql_opcode, wait_class, n, total, total_by_sql_id,
                round(n/total*100,2) percent,
@@ -36,13 +35,13 @@ SELECT *
                        sum(count(*)) over () total,
                        sum(count(*)) over (partition by sql_id,sql_opcode) total_by_sql_id
                   FROM dba_hist_active_sess_history
-                 WHERE sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
-                   AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')
-                   AND snap_id > :min_snap_id
-                   AND snap_id < :max_snap_id
-                   AND dbid = :dbid
+                 WHERE sample_time > to_date('{$_POST['startDate']}', 'DD.MM.YYYY HH24:MI:SS')
+                   AND sample_time < to_date('{$_POST['endDate']}', 'DD.MM.YYYY HH24:MI:SS')
+                   AND snap_id >= {$min_snap_id}
+                   AND snap_id <= {$max_snap_id}
+                   AND dbid = {$_POST['dbid']}
                    AND instance_number = 1
-                  {$query_mod1} 
+                  {$query_mod1}
                   {$query_mod3}
                  GROUP BY sql_id, sql_opcode, NVL({$query_mod2},'CPU')
                  ORDER BY 6 DESC)
@@ -51,23 +50,23 @@ SELECT *
  ORDER BY rank
 SQL;
 
-   } else if ($_POST['type'] === 'top-session') {
+} else if ($_POST['type'] === 'top-session') {
 
    $query = <<<SQL
 SELECT *
 FROM (SELECT session_id, program, wait_class, user_id, n, total, total_by_sid,
             round(n/total*100,2) percent,
             dense_rank() over (order by total_by_sid desc, session_id desc) as rank
-       FROM (SELECT session_id || ',' || session_serial# session_id, 
+       FROM (SELECT session_id || ',' || session_serial# session_id,
                     program, nvl({$query_mod2},'CPU') wait_class, user_id, count(*) n,
                     sum(count(*)) over () total,
                     sum(count(*)) over (partition by session_id  || ',' ||  session_serial#) total_by_sid
                FROM dba_hist_active_sess_history
-              WHERE sample_time > to_date(:start_date, 'DD.MM.YYYY HH24:MI:SS')
-                AND sample_time < to_date(:end_date, 'DD.MM.YYYY HH24:MI:SS')
-                AND snap_id > :min_snap_id
-                AND snap_id < :max_snap_id
-                AND dbid = :dbid
+              WHERE sample_time > to_date('{$_POST['startDate']}', 'DD.MM.YYYY HH24:MI:SS')
+                AND sample_time < to_date('{$_POST['endDate']}', 'DD.MM.YYYY HH24:MI:SS')
+                AND snap_id >= {$min_snap_id}
+                AND snap_id <= {$max_snap_id}
+                AND dbid = {$_POST['dbid']}
                 AND instance_number = 1 {$query_mod1}
               GROUP BY session_id || ',' || session_serial#, program, nvl({$query_mod2},'CPU'), user_id
               ORDER BY 7 desc, 1))
@@ -75,178 +74,145 @@ WHERE rank <= 10
 ORDER BY rank
 SQL;
 
-  }
+}
+$query = removeEmptyLines($query);
 
-   $start_time = microtime(true);
+$start_time = microtime(true);
 
-   $sql = str_replace(':dbid', $_POST['dbid'], $query);
-   $sql = str_replace(':min_snap_id', $min_snap_id, $sql);
-   $sql = str_replace(':max_snap_id', $max_snap_id, $sql);
-   $sql = str_replace(':start_date', "'".$start_date."'", $sql);
-   $sql = str_replace(':end_date', "'".$end_date."'", $sql);
-   $sql = removeEmptyLines($sql);   
-  
-   $statement = oci_parse($connect, $sql);
-   
-   // oci_bind_by_name($statement, ':dbid', $_POST['dbid']);
-   // oci_bind_by_name($statement, ':min_snap_id', $min_snap_id, -1, OCI_B_INT);
-   // oci_bind_by_name($statement, ':max_snap_id', $max_snap_id, -1, OCI_B_INT);
-   // oci_bind_by_name($statement, ":start_date", $start_date);
-   // oci_bind_by_name($statement, ":end_date", $end_date);
-   // oci_bind_by_name($statement, ":sum_activity", $sum_activity);
+$statement = oci_parse($connect, $query);
+oci_execute($statement);
+oci_fetch_all($statement, $results);
 
-   oci_execute($statement);
-   oci_fetch_all($statement, $results);   
-   
-   if ($_POST['type'] === 'top-sql') {
+if ($_POST['type'] === 'top-sql') {
 
-      $sqlids = array();
-      foreach($results['SQL_ID'] as $key=>$val) {
-         $sqlids[$val] = true;
-      }
+   $sqlid_list = '';
 
-      $sqlids = array_keys($sqlids);
-      $sqlid_list = '';
-
-      foreach ($sqlids as $key=>$sqlid) {
-         $sqlid_list .= "'" . $sqlid ."'," ;
-      }
-
-      $sqlid_list = rtrim($sqlid_list, ",");
-
-      if ($sqlid_list === '') {
-         exit;
-      }
-
-      $query = <<<SQL
-SELECT sql_id,
-       SUM(executions_delta) executions,
-       ROUND(SUM(s.elapsed_time_delta)/DECODE(SUM(s.executions_delta),0,1,SUM(s.executions_delta))/1e6, 2) avg_time
-  FROM dba_hist_sqlstat s
- WHERE sql_id IN ({$sqlid_list})
-   AND snap_id > {$min_snap_id}
-   AND snap_id < {$max_snap_id}
-   AND dbid = {$_POST['dbid']}
-   AND instance_number = 1
- GROUP BY sql_id
-SQL;
-
-      $statement = oci_parse($connect, $query);
-      oci_execute($statement);
-      oci_fetch_all($statement, $sqlstats_results);
-      
-      $sql_stats = array();
-      for ($i=0; $i<sizeof($sqlstats_results['SQL_ID']); $i++) {
-         $sql_stats[$sqlstats_results['SQL_ID'][$i]]['EXECUTIONS'] = $sqlstats_results['EXECUTIONS'][$i];
-         $sql_stats[$sqlstats_results['SQL_ID'][$i]]['AVG_TIME'] = $sqlstats_results['AVG_TIME'][$i];
-      }
-      
-      $query = <<<SQL
-SELECT distinct sql_id, dbms_lob.substr(sql_text,1000,1) sql_text
-  FROM dba_hist_sqltext
- WHERE sql_id IN ({$sqlid_list})
-   AND dbid = {$_POST['dbid']}
-SQL;
-
-      $statement = oci_parse($connect, $query);
-      oci_execute($statement);
-      oci_fetch_all($statement, $sql_text_results);
-     
-      $sql_text = array();
-      for ($i=0; $i<sizeof($sql_text_results['SQL_TEXT']); $i++) {
-         $sql_text[$sql_text_results['SQL_ID'][$i]]['SQL_TEXT'] = $sql_text_results['SQL_TEXT'][$i];
+   foreach($results['SQL_ID'] as $key=>$val) {
+      if (!strpos($sqlid_list, $val)) {
+         $sqlid_list .= "'" . $val ."'," ;
       }
    }
 
-   if (sizeof($results["N"]) <= 0) {
+   if ($sqlid_list === '') {
       exit;
    } else {
-      $sum_activity = $results["TOTAL"][0];
+      $sqlid_list = rtrim($sqlid_list, ",");
    }
 
-   $top = array();
-   for ($i=0; $i<sizeof($results["N"]); $i++) {
-      if ($_POST['type'] === 'top-sql') {
-         if (isset($sql_text[$results["SQL_ID"][$i]]['SQL_TEXT'])) {
-            $top[$results["SQL_ID"][$i]]["TEXT"] = $sql_text[$results["SQL_ID"][$i]]['SQL_TEXT'];
-         } else {
-            $top[$results["SQL_ID"][$i]]["TEXT"] = '';
-         }
-         $top[$results["SQL_ID"][$i]]["SQL_ID"] = $results["SQL_ID"][$i];
-         $top[$results["SQL_ID"][$i]]["SQL_OPCODE"] = $results["SQL_OPCODE"][$i];
+   $query = <<<SQL
+SELECT s.sql_id,
+       SUM(executions_delta) executions,
+       ROUND(SUM(s.elapsed_time_delta)/DECODE(SUM(s.executions_delta),0,1,SUM(s.executions_delta))/1e6, 2) avg_time,
+       dbms_lob.substr(sql_text,1000,1) sql_text
+  FROM dba_hist_sqlstat s,   
+       dba_hist_sqltext t
+ WHERE s.sql_id IN ({$sqlid_list})
+   AND snap_id >= {$min_snap_id}
+   AND snap_id <= {$max_snap_id}
+   AND s.dbid = {$_POST['dbid']}
+   AND s.instance_number = 1
+   AND s.sql_id = t.sql_id
+ GROUP   BY s.sql_id, dbms_lob.substr(sql_text,1000,1)    
+SQL;
 
-         if (isset($sql_stats[$results["SQL_ID"][$i]])) {
-            $top[$results["SQL_ID"][$i]]["AVG_TIME"] = $sql_stats[$results["SQL_ID"][$i]]["AVG_TIME"];
-            $top[$results["SQL_ID"][$i]]["EXECUTIONS"] = $sql_stats[$results["SQL_ID"][$i]]["EXECUTIONS"];;
-         } else {
-            $top[$results["SQL_ID"][$i]]["AVG_TIME"] = 'unavailable';
-            $top[$results["SQL_ID"][$i]]["EXECUTIONS"] = 'unavailable';
-         }
-
-         $top[$results["SQL_ID"][$i]]["PERCENT_TOTAL"] = $results["TOTAL_BY_SQL_ID"][$i]/$results["TOTAL"][$i]*100;
-         $top[$results["SQL_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
-      } elseif ($_POST['type'] === 'top-session') {
-         $top[$results["SESSION_ID"][$i]]["PROGRAM"] = $results["PROGRAM"][$i];
-         $top[$results["SESSION_ID"][$i]]["SESSION_ID"] = $results["SESSION_ID"][$i];
-         $top[$results["SESSION_ID"][$i]]["USERNAME"] = $results["USER_ID"][$i];
-         $top[$results["SESSION_ID"][$i]]["PERCENT_TOTAL"] = $results["TOTAL_BY_SID"][$i]/$results["TOTAL"][$i]*100;
-         $top[$results["SESSION_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
-      }
+   $statement = oci_parse($connect, $query);   
+   oci_execute($statement);
+   oci_fetch_all($statement, $sqlstats_results);
+   
+   $sqlstats = array();
+   foreach($sqlstats_results['SQL_ID'] as $row_number => $sqlid) {        
+      $sqlstats[$sqlid]['EXECUTIONS'] = $sqlstats_results['EXECUTIONS'][$row_number];
+      $sqlstats[$sqlid]['AVG_TIME'] = $sqlstats_results['AVG_TIME'][$row_number];
+      $sqlstats[$sqlid]['SQL_TEXT'] = $sqlstats_results['SQL_TEXT'][$row_number];
    }
+}
 
-   print "<table class='output'>";
+if (sizeof($results["N"]) <= 0) {
+   exit;
+} 
 
-   // Table headers
-   print "<thead><tr>";
+$sum_activity = $results["TOTAL"][0];
+
+$top = array();
+for ($i=0; $i<sizeof($results["N"]); $i++) {
    if ($_POST['type'] === 'top-sql') {
-      print "<th align='left'>SQL ID</th>";
-      print "<th width='150px' align='left'>Activity</th>";
-      print "<th align='left' nowrap>SQL Type</th>";
-      print "<th align='left' nowrap>&nbsp;&nbsp;Executions</th>";
-      print "<th align='left' nowrap>&nbsp;&nbsp;Average Time</th>";
+      $top[$results["SQL_ID"][$i]]["SQL_ID"] = $results["SQL_ID"][$i];
+      $top[$results["SQL_ID"][$i]]["SQL_OPCODE"] = $results["SQL_OPCODE"][$i];
+
+      if (isset($sqlstats[$results["SQL_ID"][$i]])) {
+         $top[$results["SQL_ID"][$i]]["AVG_TIME"] = $sqlstats[$results["SQL_ID"][$i]]["AVG_TIME"];
+         $top[$results["SQL_ID"][$i]]["EXECUTIONS"] = $sqlstats[$results["SQL_ID"][$i]]["EXECUTIONS"];
+         $top[$results["SQL_ID"][$i]]["TEXT"] = $sqlstats[$results["SQL_ID"][$i]]['SQL_TEXT'];
+      } else {
+         $top[$results["SQL_ID"][$i]]["AVG_TIME"] = 'unavailable';
+         $top[$results["SQL_ID"][$i]]["EXECUTIONS"] = 'unavailable';
+         $top[$results["SQL_ID"][$i]]["TEXT"] = '';
+      }
+
+      $top[$results["SQL_ID"][$i]]["PERCENT_TOTAL"] = $results["TOTAL_BY_SQL_ID"][$i]/$results["TOTAL"][$i]*100;
+      $top[$results["SQL_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
    } elseif ($_POST['type'] === 'top-session') {
-      print "<th align='left' nowrap>SID,Serial#&nbsp;&nbsp;</th>";
-      print "<th width='150px' align='left'>Activity</th>";
-      print "<th align='left'>User ID&nbsp;&nbsp;</th>";
-      print "<th align='left'>Program</th>";
+      $top[$results["SESSION_ID"][$i]]["PROGRAM"] = $results["PROGRAM"][$i];
+      $top[$results["SESSION_ID"][$i]]["SESSION_ID"] = $results["SESSION_ID"][$i];
+      $top[$results["SESSION_ID"][$i]]["USERNAME"] = $results["USER_ID"][$i];
+      $top[$results["SESSION_ID"][$i]]["PERCENT_TOTAL"] = $results["TOTAL_BY_SID"][$i]/$results["TOTAL"][$i]*100;
+      $top[$results["SESSION_ID"][$i]]["WAIT_CLASS"][$results["WAIT_CLASS"][$i]] = $results["PERCENT"][$i];
    }
-   print "</tr></thead>";
+}
 
-   // Rest of the table
-   foreach ($top as $position) {
-      print "<tr>";
+print "<table class='output'>";
 
-      if ($_POST['type'] === 'top-sql') {
-         print "<td><a href='#' title='".htmlspecialchars($position["TEXT"]) ."'>".$position["SQL_ID"] . "</a>&nbsp;</td>";
-      } elseif ($_POST['type'] === 'top-session') {
-         print "<td>".$position["SESSION_ID"] . "</td>";
-      }
+// Table headers
+print "<thead><tr>";
+if ($_POST['type'] === 'top-sql') {
+   print "<th align='left'>SQL ID</th>";
+   print "<th width='150px' align='left'>Activity</th>";
+   print "<th align='left' nowrap>SQL Type</th>";
+   print "<th align='left' nowrap>&nbsp;&nbsp;Executions</th>";
+   print "<th align='left' nowrap>&nbsp;&nbsp;Average Time</th>";
+} elseif ($_POST['type'] === 'top-session') {
+   print "<th align='left' nowrap>SID,Serial#&nbsp;&nbsp;</th>";
+   print "<th width='150px' align='left'>Activity</th>";
+   print "<th align='left'>User ID&nbsp;&nbsp;</th>";
+   print "<th align='left'>Program</th>";
+}
+print "</tr></thead>";
 
-      print "<td>";
-      print "<table width='100%'><tr>";
-      print "<td width='100%'>";
-      foreach ($position["WAIT_CLASS"] as $key => $value) {
-         print "<div style='background:".$_POST["eventColors"][$key].";width:$value%;float:left;'>&nbsp;</div>";
-      }
-      print "</td>";
-      print "<td><div style=''>".round($position["PERCENT_TOTAL"],2). "%</div></td>";
-      print "</td>";
-      print "</tr></table>";
+// Rest of the table
+foreach ($top as $position) {
+   print "<tr>";
 
-      if ($_POST['type'] === 'top-sql') {
-         print "<td nowrap align='left'>".get_sqltype($position["SQL_OPCODE"]) . "</td>";
-         print "<td nowrap align='right'>". $position["EXECUTIONS"] . "</td>";
-         print "<td nowrap align='right'>". number_format(round($position["AVG_TIME"],4),2,'.','') . "s</td>";
-      } elseif ($_POST['type'] === 'top-session') {
-         print "<td nowrap align='right'>". $position["USERNAME"] . "</td>";
-         print "<td nowrap>". $position["PROGRAM"] . "</td>";
-      }
-      print "</tr>";
+   if ($_POST['type'] === 'top-sql') {
+      print "<td><a href='#' title='".htmlspecialchars($position["TEXT"]) ."'>".$position["SQL_ID"] . "</a>&nbsp;</td>";
+   } elseif ($_POST['type'] === 'top-session') {
+      print "<td>".$position["SESSION_ID"] . "</td>";
    }
-   print "</table>";
 
-   $end_time = microtime(true);
+   print "<td>";
+   print "<table width='100%'><tr>";
+   print "<td width='100%'>";
+   foreach ($position["WAIT_CLASS"] as $key => $value) {
+      print "<div style='background:".$_POST["eventColors"][$key].";width:$value%;float:left;'>&nbsp;</div>";
+   }
+   print "</td>";
+   print "<td><div style=''>".round($position["PERCENT_TOTAL"],2). "%</div></td>";
+   print "</td>";
+   print "</tr></table>";
 
-   print "<div align='right'><font style='font-family: Tahoma,Verdana,Helvetica,sans-serif;font-size:9px' color='gray'>Total Sample Count: $sum_activity, Returned in: ".round($end_time - $start_time,2) . "s</font></div>";
+   if ($_POST['type'] === 'top-sql') {
+      print "<td nowrap align='left'>".get_sqltype($position["SQL_OPCODE"]) . "</td>";
+      print "<td nowrap align='right'>". $position["EXECUTIONS"] . "</td>";
+      print "<td nowrap align='right'>". number_format(round($position["AVG_TIME"],4),2,'.','') . "s</td>";
+   } elseif ($_POST['type'] === 'top-session') {
+      print "<td nowrap align='right'>". $position["USERNAME"] . "</td>";
+      print "<td nowrap>". $position["PROGRAM"] . "</td>";
+   }
+   print "</tr>";
+}
+print "</table>";
+
+$end_time = microtime(true);
+
+print "<div align='right'><font style='font-family: Tahoma,Verdana,Helvetica,sans-serif;font-size:9px' color='gray'>Total Sample Count: $sum_activity, Returned in: ".round($end_time - $start_time,2) . "s</font></div>";
 
 ?>
